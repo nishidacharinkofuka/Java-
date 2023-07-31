01537bbb99db76c816767ec81ad3b09551d38493
fuka <seven.f.nishida@gmail.com>
2023-07-31 23:06:13 +0900

    1.3カリキュラム　新規作成

---
+++ HelloWorld_FamilynameFirstname/src/sample/SelfIntroduction.java
@@ -0,0 +1,29 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//氏名
+		String name = "西田 風香";
+		
+		//年齢
+		int age = 26;
+		
+		//趣味
+		String hobby = "漫画";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		
+		
+
+	}
+
+}
