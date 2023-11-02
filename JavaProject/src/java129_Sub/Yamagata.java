8f3217a82a6a838ca1971cc6a80faf1786a2e036
fuka <seven.f.nishida@gmail.com>
2023-11-03 00:15:50 +0900

    テスト

---
+++ JavaProject/src/java129_Sub/Yamagata.java
@@ -0,0 +1,14 @@
+package java129_Sub;
+
+public class Yamagata extends Prefecture{
+
+	public Yamagata(){
+		
+		this.number = 5;		// 番号
+		this.ken = "山形県";	// 県名
+		this.cap ="山形市";	// 県庁所在地
+		this.area = 9323;		// 面積
+		
+	}
+
+}
