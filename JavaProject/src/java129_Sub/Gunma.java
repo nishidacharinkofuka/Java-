ca0db0b1a217b5ddae87a17723dd24b1a8940489
fuka <seven.f.nishida@gmail.com>
2023-11-03 02:07:08 +0900

    テスト

---
+++ JavaProject/src/java129_Sub/Gunma.java
@@ -0,0 +1,14 @@
+package java129_Sub;
+
+public class Gunma extends Prefecture{
+
+	public Gunma(){
+		
+		this.number = 8;		// 番号
+		this.ken = "群馬県";	// 県名
+		this.cap ="前橋市";		// 県庁所在地
+		this.area = 6362;		// 面積
+		
+	}
+
+}
