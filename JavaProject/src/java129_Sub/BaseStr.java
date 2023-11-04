9b3888588f53fc111887f751c90bafdb96fc1863
fuka <seven.f.nishida@gmail.com>
2023-11-04 22:35:25 +0900

    11/4 Java1.29 提出

---
+++ JavaProject/src/java129_Sub/BaseStr.java
@@ -0,0 +1,13 @@
+package java129_Sub;
+
+public class BaseStr extends Base {		// サブクラス
+	
+	public void Str() {		// 出力
+		
+		System.out.println("\n 都道府県名：" + this.name
+				+ "\n県庁所在地：" + this.cap
+				+ "\n面積：" + this.area);
+		
+	}
+
+}
